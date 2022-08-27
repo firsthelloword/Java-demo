6fd69fcb4a88f510381d969f6b4971d1941f42f8
zhangyongkai <1184908070@qq.com>
2022-08-27 12:24:27 +0800

    初始化项目

---
+++ api/src/test/java/zykdemo/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package zykdemo.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
